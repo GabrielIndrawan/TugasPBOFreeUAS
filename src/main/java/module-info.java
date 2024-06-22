297a7b74a4110995ddc158f364b6b948d337e585
GabrielIndrawan <Abelepic3@gmail.com>
2024-06-22 13:49:51 +0700

    First Commit Tugas PBO Free UTS

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tugaspbofreeuas {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.tugaspbofreeuas to javafx.fxml;
+    exports com.example.tugaspbofreeuas;
+}
